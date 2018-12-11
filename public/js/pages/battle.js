da18fd0caef8dc5b213758e883ac4f9c9d3bf745
Camille <puyocamille@gmail.com>
2018-12-11 15:24:46 +0000

    played with css to display results

--- public/js/pages/battle.js
+++ public/js/pages/battle.js
@@ -7,22 +7,16 @@ const {access_token, state} = getHashParams();
 const storedState = localStorage.getItem(STATE_KEY);
 
 
-const outputTemplate = ({display_name, id, email, uri, external_urls, images, country}) =>`<h2>Logged in as </h2>
-  <div class="media">
-    <div class="pull-left">
-      <img class="media-object" width="150" src="">
-    </div>
-    <div class="media-body">
-      <dl class="dl-horizontal">
-        <dt>Display name</dt><dd class="clearfix">${display_name}</dd>
+const outputTemplate = ({display_name, id, email, uri, external_urls, country}) =>`<h3>Logged in as </h3>
+  <div>
+      <dl>
+        <dt>Display name</dt><dd>${display_name}</dd>
         <dt>Id</dt><dd>${id}</dd>
         <dt>Email</dt><dd>${email}</dd>
         <dt>Spotify URI</dt><dd><a href="${uri}">${uri}</a></dd>
         <dt>Link</dt><dd><a href="${external_urls.spotify}">${external_urls.spotify}</a></dd>
-        <dt>Profile Image</dt><dd class="clearfix"><a href=""></a></dd>
         <dt>Country</dt><dd>${country}</dd>
       </dl>
-    </div>
   </div>`
 
 
@@ -43,7 +37,7 @@ const playAgain = document.getElementById('playAgain');
 const button1 = document.getElementById('button1');
 
 
-
+//SEARCH FOR ARTIST 1
 async function getArtists() {
 try {
 const userInput1 = document.getElementById('artist1').value;
@@ -68,7 +62,7 @@ const data = await response.json();
 
 
 
-                             
+// SEARCH FOR ARTIST 2                             
 async function getArtists2() {
 try {
 const userInput2 = document.getElementById('artist2').value;
@@ -89,22 +83,12 @@ const data = await response.json();
 
 document.getElementById('button2').addEventListener('click', getArtists2);
 
+
+
+// MAKE THEM FIGHT!
 async function artistsPop() {
 try {
-const userInput2 = document.getElementById('artist2').value;
-const response2 = await fetch(`https://api.spotify.com/v1/search?q=${userInput2}/&type=artist&limit=1`, {
-      headers: {
-        'Authorization': `Bearer ${access_token}`
-      }
-    });
-const data2 = await response2.json();
-let pop2 = `${data2.artists.items[0].popularity}`
-let musicianName2 = `${data2.artists.items[0].name}`
-function displayInfo2() {
-     return pop2, musicianName2;
-}
-   displayInfo2();
-
+// INFO ABOUT ARTIST 1
 const userInput1 = document.getElementById('artist1').value;
 const response1 = await fetch(`https://api.spotify.com/v1/search?q=${userInput1}/&type=artist&limit=1`, {
       headers: {
@@ -114,27 +98,45 @@ const response1 = await fetch(`https://api.spotify.com/v1/search?q=${userInput1}
 const data1 = await response1.json();
 let pop1 = `${data1.artists.items[0].popularity}`
 let musicianName1 = `${data1.artists.items[0].name}`
+let musicianPicture1 = `${data1.artists.items[0].images[0].url}`
 function displayInfo1() {
      return pop1, musicianName1;
 }
    displayInfo1();
-
+// INFO ABOUT ARTIST 2    
+const userInput2 = document.getElementById('artist2').value;
+const response2 = await fetch(`https://api.spotify.com/v1/search?q=${userInput2}/&type=artist&limit=1`, {
+      headers: {
+        'Authorization': `Bearer ${access_token}`
+      }
+    });
+const data2 = await response2.json();
+let pop2 = `${data2.artists.items[0].popularity}`
+let musicianName2 = `${data2.artists.items[0].name}`
+let musicianPicture2 = `${data2.artists.items[0].images[0].url}`
+function displayInfo2() {
+     return pop2, musicianName2;
+}
+   displayInfo2();
+ // THE BATTLE   
     if (pop1 > pop2) {
-    console.log(musicianName1, pop1)
+    modalBox.innerHTML = `<img src="${musicianPicture1}"> <br> <span>${musicianName1}</span> won with a popularity of ${pop1}`
 }    else if (pop2 > pop1) {
-    console.log(musicianName2, pop2)
+    modalBox.innerHTML = `<img src="${musicianPicture2}"> <br> <span>${musicianName2}</span> won with a popularity of ${pop2}`
 } else if (pop2 === pop1) {
-    console.log("Both win!");
-
+    modalBox.innerHTML = `Both win with a popularity of ${pop2}`;
+}
 
 }
-} catch (err){ 
+
+ catch (err){ 
     console.log(err);
  }
 }
 
-playAgain.addEventListener('click', () => location.reload())
 
-
-        
 document.getElementById('fight').addEventListener('click', artistsPop);
+
+
+// CLEAR TO PLAY AGAIN
+playAgain.addEventListener('click', () => location.reload())
