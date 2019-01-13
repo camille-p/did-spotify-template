57c0b0d58b52231324c519ce7c31c01b9a8e58d0
Camille <puyocamille@gmail.com>
2019-01-13 09:59:21 +0000

    changed folder name public to docs

--- public/js/helpers/constants.js
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = 'https://the-spotify-battle.herokuapp.com/';
-//export const BASE_URL = 'http://localhost:8888/';
-export const CLIENT_ID = '9669e157beff48d380aec8db3f4b6109';
-export const STATE_KEY = 'spotify_auth_state';
